38a1c1f488815b8e8218c9de9221ba4f7205f7ef
Javoxirbey <rabbonayevjavoxir1078@gmail.com>
2022-03-05 07:17:33 -0930

    Initialize project using Create React App

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Typography,CssBaseline} from "@mui/material";
+
+function Home(props) {
+
+    return (
+        <div>
+            <CssBaseline/>
+
+
+        </div>
+    );
+}
+
+export default Home;
